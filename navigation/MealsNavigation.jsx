ce961f36bc9c868257179e90930cdec8ce40b5ac
Omar Portillo <OmrPrt@aol.com>
2020-04-24 04:13:10 +0100

    updated to react navigation v5

---
+++ navigation/MealsNavigation.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import CategoriesScreen from '../screens/CategoriesScreen';
+import CategoryMealsScreen from '../screens/CategoryMealsScreen';
+import MealDetailScreen from '../screens/MealDetailScreen';
+
+const Stack = createStackNavigator();
+
+const MealsNavigator = (props) => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Categories' component={CategoriesScreen} />
+        <Stack.Screen name='CategoryMeals' component={CategoryMealsScreen} />
+        <Stack.Screen name='MealDetails' component={MealDetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+// const MealsNavigator = createStackNavigator({
+//   Categories: CategoriesScreen,
+//   CategoryMeals: {
+//     screen: CategoryMealsScreen
+//   },
+//   MealDetail: MealDetailScreen
+// });
+
+export default MealsNavigator;
